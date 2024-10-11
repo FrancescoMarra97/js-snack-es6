c9a853d998042f0d9b054ab288e3d682b128a82c
FrancescoMarra97 <francescomarra192@gmail.com>
2024-10-11 14:10:21 +0200

    array di biciclette

--- snack 1/main.js
+++ snack 1/main.js
@@ -1,1 +1,27 @@
-console.log("it works");
+/* Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+ */
+
+const biciclette = [
+    { 
+        nome: "Bianchi",
+        peso: 8 
+    },
+    {
+         nome: "Trek", 
+         peso: 7.5 
+    },
+    { 
+        nome: "Cannondale", 
+        peso: 7.8 
+    },
+    { 
+        nome: "Specialized", 
+        peso: 6.9 
+    },
+    { 
+        nome: "Pinarello", 
+        peso: 7.3 
+    }
+];
